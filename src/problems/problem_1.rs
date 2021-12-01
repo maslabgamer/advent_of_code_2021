ec4719878946af22c3f49a3d036e23a29e9e1de5
maslabgamer <maslabgamer@gmail.com>
2021-12-01 12:20:16 -0800

    Changes solution to use single function

--- src/problems/problem_1.rs
+++ src/problems/problem_1.rs
@@ -1,35 +1,26 @@
-fn count_increases(input: &Vec<i32>) -> u32 {
+fn window_increase(input: &Vec<i32>, window_size: usize) -> u32 {
     let mut increases = 0;
-    let mut previous = input[0];
-    for i in 1..input.len() {
-        let current = input[i];
-        increases += (current > previous) as u32;
-        previous = current;
-    }
-    increases
-}
+    let mut previous: i32 = (input[0..window_size]).iter().sum();
 
-fn three_measurement_sliding_window(input: &Vec<i32>) -> u32 {
-    let mut increases = 0;
-    let mut previous = input[0] + input[1] + input[2];
-    for i in 3..input.len() {
-        let current = input[i] + input[i - 1] + input[i - 2];
+    for i in window_size..input.len() {
+        let current: i32 = (input[i - (window_size - 1)..=i]).iter().sum();
         increases += (current > previous) as u32;
         previous = current;
     }
+
     increases
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::problems::problem_1::{count_increases, three_measurement_sliding_window};
+    use crate::problems::problem_1::window_increase;
 
     #[test]
     fn part_one() {
         let input: Vec<i32> = include_str!("../../resources/problem_1_input.txt").lines()
             .map(|number| number.parse::<i32>().unwrap())
             .collect();
-        assert_eq!(1832, count_increases(&input));
+        assert_eq!(1832, window_increase(&input, 1));
     }
 
     #[test]
@@ -37,6 +28,6 @@ mod tests {
         let input: Vec<i32> = include_str!("../../resources/problem_1_part_two_input.txt").lines()
             .map(|number| number.parse::<i32>().unwrap())
             .collect();
-        assert_eq!(1858, three_measurement_sliding_window(&input));
+        assert_eq!(1858, window_increase(&input, 3));
     }
 }

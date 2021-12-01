224d7aa45f1dc54972564642c1ba602da73889c8
maslabgamer <maslabgamer@gmail.com>
2021-12-01 12:12:26 -0800

    Adds problem 1 part 2

--- src/problems/problem_1.rs
+++ src/problems/problem_1.rs
@@ -9,9 +9,20 @@ fn count_increases(input: &Vec<i32>) -> u32 {
     increases
 }
 
+fn three_measurement_sliding_window(input: &Vec<i32>) -> u32 {
+    let mut increases = 0;
+    let mut previous = input[0] + input[1] + input[2];
+    for i in 3..input.len() {
+        let current = input[i] + input[i - 1] + input[i - 2];
+        increases += (current > previous) as u32;
+        previous = current;
+    }
+    increases
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::problems::problem_1::count_increases;
+    use crate::problems::problem_1::{count_increases, three_measurement_sliding_window};
 
     #[test]
     fn part_one() {
@@ -19,5 +30,14 @@ mod tests {
             .map(|number| number.parse::<i32>().unwrap())
             .collect();
         assert_eq!(1832, count_increases(&input));
+    }
+
+    #[test]
+    fn part_two() {
+        let input: Vec<i32> = include_str!("../../resources/problem_1_part_two_input.txt").lines()
+            .map(|number| number.parse::<i32>().unwrap())
+            .collect();
+
+        println!("{}", three_measurement_sliding_window(&input));
     }
 }
